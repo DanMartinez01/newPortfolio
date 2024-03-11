0a3635ea68cab631f7c723235dfca84179757b64
Daniela <martinezdl_@hotmail.com>
2024-03-11 12:33:15 -0300

    add data to aboutMe

--- data/aboutMeData.js
+++ data/aboutMeData.js
@@ -3,10 +3,10 @@ import { v4 as uuidv4 } from "uuid";
 export const aboutMeData = [
   {
     id: uuidv4(),
-    bio: "👋 Hi, I'm Daniela Martinez, a Fullstack Developer and former English teacher. I've freelanced in React/Next.js for various businesses, turning ideas into functional websites. I love learning new things independently and enjoy creating innovative solutions. Let's build something awesome together! 🚀✨",
+    bio: "👋 Hi, I'm Daniela Martinez, a Fullstack Developer and former English teacher. I've freelanced in React/Next.js for various businesses, turning ideas into functional websites. I am currently part of Programadores Argentina tech community. I love learning new things independently and enjoy creating innovative solutions. Let's build something awesome together! 🚀✨",
   },
   {
     id: uuidv4(),
-    bio: "I have experience in React, NextJs, Typescript,PostgreSQL, among others",
+    bio: "I have experience in React, NextJs, Typescript,PostgreSQL,TailwindCss, among others",
   },
 ];
