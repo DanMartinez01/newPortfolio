50a9519396c6674855b71511f020883c9e9359f4
Daniela <martinezdl_@hotmail.com>
2024-05-02 11:52:04 -0300

    remove unused imports

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,5 @@
-import Link from "next/link";
 import PagesMetaHead from "../components/PagesMetaHead";
 import ProjectsGrid from "../components/projects/ProjectsGrid";
-import Button from "../components/reusable/Button";
 import AppBanner from "../components/shared/AppBanner";
 import Chat from "../components/Chat";
 
