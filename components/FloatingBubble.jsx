fe71bdc10ef082071918166065caf2f920b9d6db
Daniela <martinezdl_@hotmail.com>
2024-05-05 20:54:36 -0300

    creating Floating Bubble for Chat

---
+++ components/FloatingBubble.jsx
@@ -0,0 +1,34 @@
+// FloatingBubble.jsx
+import React, { useState } from "react";
+import Chat from "./Chat";
+
+function FloatingBubble() {
+  const [isChatOpen, setIsChatOpen] = useState(false);
+
+  const toggleChat = () => {
+    setIsChatOpen((prevIsOpen) => !prevIsOpen);
+    if (!isChatOpen) {
+      // Calculate the position to scroll to
+      const scrollPosition = document.body.scrollHeight + 200; // Adjust 100 to your preference
+
+      // Scroll to the calculated position
+      window.scrollTo({ top: scrollPosition, behavior: "smooth" });
+    }
+  };
+
+  return (
+    <div>
+      {!isChatOpen && (
+        <div
+          className="fixed bottom-4 right-4 bg-blue-500 p-3 rounded-full text-white cursor-pointer"
+          onClick={toggleChat}
+        >
+          Chat with me
+        </div>
+      )}
+      {isChatOpen && <Chat initialMessage={"Ask me a question!"} />}
+    </div>
+  );
+}
+
+export default FloatingBubble;
