6b85149bcbd8fc07fa8757694e42ef106e16a0b4
Daniela <martinezdl_@hotmail.com>
2024-05-05 20:55:19 -0300

    import new components

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,45 @@
+// import PagesMetaHead from "../components/PagesMetaHead";
+// import ProjectsGrid from "../components/projects/ProjectsGrid";
+// import AppBanner from "../components/shared/AppBanner";
+// import Chat from "../components/Chat";
+// import dynamic from "next/dynamic";
+
+// const Scene = dynamic(() => import("../components/Scene"), {
+//   ssr: false,
+// });
+
+// export default function Home() {
+//   return (
+//     <div className="container mx-auto">
+//       <Scene />
+//       <PagesMetaHead title="Home" />
+//       <AppBanner />
+//       <ProjectsGrid />
+//       <Chat initialMessage={"Ask me a question!"} />
+//     </div>
+//   );
+// }
+
+import React from "react";
 import PagesMetaHead from "../components/PagesMetaHead";
 import ProjectsGrid from "../components/projects/ProjectsGrid";
 import AppBanner from "../components/shared/AppBanner";
 import Chat from "../components/Chat";
+import FloatingBubble from "../components/FloatingBubble";
+import dynamic from "next/dynamic";
+
+const Scene = dynamic(() => import("../components/Scene"), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
     <div className="container mx-auto">
+      <Scene />
       <PagesMetaHead title="Home" />
       <AppBanner />
       <ProjectsGrid />
-      <Chat initialMessage={"Ask me a question!"} />
+      <FloatingBubble />
     </div>
   );
 }
